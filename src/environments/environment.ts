10ede0e640e78f76b5da134b66aec49723b723d9
Salih Tarık ERDOĞAN <salihtarike@gmail.com>
2023-06-07 01:54:32 +0300

    stwebfinal(firebase connected)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyAZuvZGomT6TRpNZubQkQX1-hQA484ljmg',
+    authDomain: 'stewebfinal-8efb3.firebaseapp.com',
+    projectId: 'stewebfinal-8efb3',
+    storageBucket: 'stewebfinal-8efb3.appspot.com',
+    messagingSenderId: '946289608138',
+    appId: '1:946289608138:web:3f84a7b8f702c8f76b3b33',
+    measurementId: 'G-JK0FLBN5CC',
+  },
 };
 
 /*
